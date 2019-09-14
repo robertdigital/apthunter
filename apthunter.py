7f02db3b5c1c23389f256c7bd1a887967b192885
Wade Wesolowsky <wadew@false.ca>
2019-09-13 21:29:54 -0600

    Open a connection to Elasticsearch

--- apthunter.py
+++ apthunter.py
@@ -10,7 +10,7 @@ import json
 
 # Use the elasticsearch library to simplify accessing
 # https://elasticsearch-py.readthedocs.io/en/6.3.1/
-import elasticsearch
+from elasticsearch import Elasticsearch
 
 # This might help to simplify things, but not necessarily
 # https://pypi.org/project/elasticsearch-dsl/
@@ -38,37 +38,46 @@ parser.add_argument("-p",
 parser.add_argument("-ht",
 	"--honeytrap",
 	help="Search the honeytrap* index in Elasticsearch.  This index will contain HoneyTrap status messages and Honeypot connection attempts.",
-	type=json.loads)
+	type=json.loads,
+	dest="honeytrap")
 parser.add_argument("-log",
 	"--logstash",
 	help="Search the logstash-* index in Elasticsearch.  This index will contain Zeek network traffic monitoring information.",
-	type=json.loads)
+	type=json.loads,
+	dest="logstash")
 parser.add_argument("-pf",
 	"--pfsense",
 	help="Search the pfsense-* index in Elasticsearch.  This index will contain firewall status messages and Snort alerts.",
-	type=json.loads)
+	type=json.loads,
+	dest="pfsense")
 parser.add_argument("-ss",
 	"--sweetsecurity",
 	help="Search the sweet_security index in Elasticsearch.  This index will contain detected device information and port scans.",
-	type=json.loads)
+	type=json.loads,
+	dest="sweetsecurity")
 parser.add_argument("-ssa",
 	"--sweetsecurityalerts",
 	help="Search the sweet_security_alerts index in Elasticsearch.  This index will contain new (unique) Sweet Security log events.",
-	type=json.loads)
+	type=json.loads,
+	dest="sweetsecurityalerts")
 parser.add_argument("-t",
 	"--tardis",
 	help="Search the tardis index in Elasticsearch.  This index will contain historical hosts, IP addresses, and websites.",
-	type=json.loads)
+	type=json.loads,
+	dest="tardis")
 parser.add_argument("-wa",
 	"--wazuhalerts",
 	help="Search the wazuh-alerts-3.x-* index in Elasticsearch.  This index will contain log events above the alert thresholds in Wazuh.",
-	type=json.loads)
+	type=json.loads,
+	dest="wazuhalerts")
 parser.add_argument("-wm",
 	"--wazuhmonitoring",
 	help="Search the wazuh-monitoring-3.x-* index in Elasticsearch.  This index will contain all Wazuh monitoring logs.",
-	type=json.loads)
+	type=json.loads,
+	dest="wazuhmonitoring")
 parser.add_argument("--debug",
-	help="Outputs useful information to find errors.")
+	help="Outputs useful information to find errors.",
+	dest="debug")
 
 # This gets the arguments!
 args = parser.parse_args()
@@ -86,3 +95,8 @@ except ValueError:
 if (args.port < 1 or args.port > 65535):
 	print("Port is invalid: %i" % args.port)
 	raise SystemExit
+
+# Open an Elasticsearch connection using the argument
+es = Elasticsearch([
+    {'host': args.server, 'port': args.port, 'url_prefix': 'es', 'use_ssl': False},
+])
